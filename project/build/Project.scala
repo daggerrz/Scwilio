ec0227e958d00f3863fe5731508da0819b36a71d
Dag Liodden <dag.liodden@giantleap.no>
2010-12-01 00:32:46 +0100

    Scala 2.8.1. ++

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,9 +1,12 @@
 import sbt._
 
-class Project(info: ProjectInfo) extends DefaultWebProject(info) {
+class Project(info: ProjectInfo) extends DefaultProject(info) {
 
+  override def packageSrcJar = defaultJarPath("-sources.jar")
+  val sourceArtifact = Artifact.sources(artifactID)
+  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
 
-  class ScwilioModule(info: ProjectInfo) extends DefaultWebProject(info) {
+  class ScwilioModule(info: ProjectInfo) extends DefaultProject(info) {
     val scalaToolsRepo = "Scala Tools Repo" at "http://scala-tools.org/repo-releases/"
     val LOGBACK_VERSION = "0.9.24"
     val SLF4J_VERSION = "1.6.1"
@@ -20,7 +23,7 @@ class Project(info: ProjectInfo) extends DefaultWebProject(info) {
       "org.slf4j" % "slf4j-api" % "1.6.1",
       "ch.qos.logback" % "logback-classic" % "0.9.26",
       "org.scalatest" % "scalatest" % "1.2" % "test",
-      "org.scala-tools.testing" %% "specs" % "1.6.5" % "test",
+      "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test",
       "org.mockito" % "mockito-core" % "1.7"
 
       )
---
+++ unfiltered/src/main/scala/scwilio/uf/server.scala
@@ -0,0 +1,11 @@
+package scwilio.uf
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: liodden
+ * Date: 30.11.10
+ * Time: 23.26
+ * To change this template use File | Settings | File Templates.
+ */
+
+class server
