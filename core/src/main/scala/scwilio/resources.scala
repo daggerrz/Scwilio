606f910ea3d4c0e0f7ec1c618638a7f93b798b1a
Dag Liodden <dag.liodden@giantleap.no>
2010-12-09 21:29:28 +0100

    Typo

--- core/src/main/scala/scwilio/resources.scala
+++ core/src/main/scala/scwilio/resources.scala
@@ -8,7 +8,7 @@ case class ConferenceParticipant(callId: String, muted: Boolean)
 case class IncomingNumber(sid: String, number: Phonenumber, config: IncomingNumberConfig)
 
 /**
- * Configuration for an incoming phonenumber. When used for changing configuration,
+ * Configuration for an incoming phone number. When used for changing configuration,
  * non-specified options will remain unchanged.
  */
 case class IncomingNumberConfig(
