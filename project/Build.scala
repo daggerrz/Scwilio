f9faa6641095545950a75a3663afd9e2acaea8be
Dag Liodden <dag@liodden.no>
2012-10-01 23:13:23 -0400

    Update project internal deps.

--- project/Build.scala
+++ project/Build.scala
@@ -4,10 +4,10 @@ import Keys._
 object BuildSettings {
   val buildOrganization = "org.scwilio"
   val buildVersion      = "0.1.1-SNAPSHOT"
-  val buildScalaVersion = "2.9.1"
+  val buildScalaVersion = "2.9.2"
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
-    crossScalaVersions := Seq("2.8.1", "2.9.0-1", "2.9.1"),
+    crossScalaVersions := Seq("2.9.1", "2.9.2"),
     organization := buildOrganization,
     version      := buildVersion,
     scalaVersion := buildScalaVersion
@@ -21,7 +21,9 @@ object ScwilioBuild extends Build {
   def scwilioProject(dir: String) =
     Project(dir, file(dir), settings = buildSettings)
 
-  lazy val core = scwilioProject("core")
+  lazy val core =       scwilioProject("core")
   lazy val unfiltered = scwilioProject("unfiltered") dependsOn(core)
-  lazy val examples = scwilioProject("examples") dependsOn(unfiltered)
+  lazy val examples =   scwilioProject("examples")   dependsOn(unfiltered)
+
+  lazy val root = Project("root", base = file(".")) aggregate (core, unfiltered, examples)
 }
